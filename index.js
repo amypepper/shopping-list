976b483cbf1eb5ef0e9759a18f836e6d763ac5df
Amy Pepper <amyandmicahpepper@gmail.com>
2019-12-30 07:37:35 -0800

    got plain language back

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// find the parent of the input text field I want to target
+// bind to it
+// prevent default submit behavior
+// grab the text entered into the input
+// store it
+// insert it into a new <li> element with the proper elements and classes
+// traverse to after the last li and place it there
+
+// listen for when the 'Delete' button is clicked
+// use .closest to target the li itself
+// use .remove method to permanently remove the <li> 
+
+// listen for when the 'Check' button is clicked
+// use .parent and .siblings to traverse to the span element with list item text 
+// toggle the class to add or remove the span element to call the CSS that strikes through event.currentTarget
+
 'use strict'
 
 function addToList() {
